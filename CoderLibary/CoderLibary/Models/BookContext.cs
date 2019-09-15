7349b2fb5ccbde292d6725ea15fa7ae2a521e873
= <=>
2019-09-15 18:39:41 +0700

    test

---
+++ CoderLibary/CoderLibary/Models/BookContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CoderLibary.Models
+{
+    public class BookContext: DbContext
+    {
+        public BookContext() : base("CoderLibary") { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ CoderLibary/CoderLibary/Models/BookDatabaseInitializer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CoderLibary.Models
+{
+    public class BookDatabaseInitializer: DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext context)
+        {
+            GetCategories().ForEach(c => context.Categories.Add(c));
+            GetBooks().ForEach(p => context.Books.Add(p));
+        }
+
+        private static List<Category> GetCategories()
+        {
+            var categories = new List<Category>{
+                new Category
+                {
+                    CategoryID = 1,
+                    CategoryName = "ProGram"
+                },
+                new Category
+                {
+                    CategoryID = 2,
+                    CategoryName = "WebSite"
+                },
+                new Category
+                {
+                    CategoryID = 3,
+                    CategoryName = "Network Security"
+                },
+                new Category
+                {
+                    CategoryID = 4,
+                    CategoryName = "Graphics"
+                }
+            };
+            return categories;
+        }
+
+        private static List<Book> GetBooks()
+        {
+            var books = new List<Book>
+            {    
+                //book 1
+                 new Book
+                 {
+                     BookID = 1,
+                     BookName = "ProGram C++",
+                     Description = "Basic ProGram",
+                     ImagePath ="sach1.jpg",
+                     UnitPrice = 4f,
+                     CategoryID = 1
+                 }
+                };
+            return books;
+        }
+    }
+}
